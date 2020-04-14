79762c645ac0c58f5414981653f71b7ceab6e2fe
Vikrant Kataria <vik7am@gmail.com>
2020-04-14 15:21:28 +0530

    Added Intent to select file

--- app/src/main/java/com/vikrant/extensionmanager/MainActivity.java
+++ app/src/main/java/com/vikrant/extensionmanager/MainActivity.java
@@ -2,13 +2,25 @@ package com.vikrant.extensionmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OpenFileManager(View view) {
+        intent = new Intent();
+        intent.setType("*/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(Intent.createChooser(intent, "Select file"), 1);
     }
 }
